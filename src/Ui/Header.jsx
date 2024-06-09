0580995732b94970c13d934af25157f395ae0bb8
varmanli <varmanliamirhosein@gmail.com>
2024-06-09 17:36:35 +0330

    update 9

--- src/Ui/Header.jsx
+++ src/Ui/Header.jsx
@@ -9,11 +9,11 @@ function Header() {
   }
   return (
     <div className="flex justify-between items-center p-5  font-semibold">
-      <div className="w-[150px] lg:w-[270px] cursor-pointer">
+      <div className="w-[150px] lg:w-[270px] cursor-pointer mb-2">
         <img src={logo} alt="logo" />
       </div>
       <button
-        className="flex justify-center items-center gap-2 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        className="flex justify-center items-center gap-2 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm md:text-sm px-2  md:px-5 py-2.5 text-center me-2 mb-2"
         onClick={randomSongHandler}
       >
         <p>پخش تصادفی موسیقی</p>
