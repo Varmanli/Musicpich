6230ca4ff00816803f88de67ef6124303608cdb8
varmanli <varmanliamirhosein@gmail.com>
2024-12-07 21:36:42 +0330

    fix hero section

--- src/Ui/PlayButton.jsx
+++ src/Ui/PlayButton.jsx
@@ -3,7 +3,7 @@ import { FaPlay } from "react-icons/fa";
 
 function PlayButton() {
   return (
-    <button className="flex justify-center items-center gap-2 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-xs md:text-sm px-2   md:px-5 py-2.5 text-center me-2 mb-2">
+    <button className="flex justify-center items-center gap-2 font-shabnam text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-xs md:text-sm px-2   md:px-5 py-2.5 text-center me-2 mb-2">
       <p>پخش</p>
       <FaPlay />
     </button>
--- src/Ui/SliderHeader.jsx
+++ src/Ui/SliderHeader.jsx
@@ -7,7 +7,7 @@ function SliderHeader() {
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
         <div className="relative z-10">
           <Header />
-          <div className="flex flex-col justify-center items-center gap-10">
+          <div className="flex flex-col justify-center items-center gap-10 mt-[100px] md:mt-[50px]">
             <h1 className="font-bold text-xl lg:text-3xl  text-center font-shabnam">
               جایی که موسیقی زنده می‌شود! <br /> با ما، صدای رویاهایت را کشف کن
             </h1>
