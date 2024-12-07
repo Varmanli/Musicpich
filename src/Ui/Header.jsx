f10a4ad0241319684c40f743daec032daf1fc3ed
varmanli <varmanliamirhosein@gmail.com>
2024-12-07 19:51:27 +0330

    fix header

--- src/Ui/Header.jsx
+++ src/Ui/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
         <img src={logo} alt="logo" />
       </div>
       <button
-        className="flex justify-center items-center gap-2 lg:ml-20 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm md:text-sm px-2  md:px-5 py-2.5 text-center me-2 mb-2"
+        className="md:flex justify-center hidden  items-center gap-2 lg:ml-20 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm md:text-sm px-2  md:px-5 py-2.5 text-center me-2 mb-2"
         onClick={randomSongHandler}
       >
         <p>پخش تصادفی موسیقی</p>
--- src/Ui/SliderHeader.jsx
+++ src/Ui/SliderHeader.jsx
@@ -35,7 +35,7 @@ function SliderHeader() {
         <div className="relative z-10">
           <Header />
           <div className="flex flex-col justify-center items-center gap-10">
-            <h1 className="font-bold text-lg lg:text-3xl  text-center font-shabnam">
+            <h1 className="font-bold text-xl lg:text-3xl  text-center font-shabnam">
               جایی که موسیقی زنده می‌شود! <br /> با ما، صدای رویاهایت را کشف کن
             </h1>
             <button className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-semibold shadow-2xl shadow-green-600 rounded-lg text-xs md:text-sm px-2   md:px-5 py-2.5 text-center me-2 mb-2">
